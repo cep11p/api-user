c845a0447151fbca057a809051499810a4b9b3a2
carlos <cep11p@gmail.com>
2020-02-18 11:33:09 -0300

    Se  configura la contraseña para ingresar a bd

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host='.getenv('DB_HOST').';dbname='.getenv('DB_NAME'),
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
