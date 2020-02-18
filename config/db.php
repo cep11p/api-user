df88cc15a6be100c1974784dd355f57b7f862086
carlos <cep11p@gmail.com>
2020-02-17 21:13:53 -0300

    Configuraciones para el arranque de la apiRest

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host='.getenv('DB_HOST').';dbname='.getenv('DB_NAME'),
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
